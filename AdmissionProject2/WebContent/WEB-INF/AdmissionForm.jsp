ed233377927edec17a72b639f1e70bc458a509ae
Nagabhushan M D <45279279+Nagabushangowdamd@users.noreply.github.com>
2019-08-02 23:18:49 +0530

    Add files via upload

---
+++ AdmissionProject2/WebContent/WEB-INF/AdmissionForm.jsp
@@ -0,0 +1,16 @@
+
+<html>
+
+<body>
+<h1>STUDENT ADMISSION FORM ENGINEER COURSES</h1>
+<form action="/AdmissionProject2/submitAdmissionForm.html" method="post">
+<p> 
+Student Name : <input type="text" name="name"/>
+</p>
+<p> 
+Student's Hobby : <input type="text" name="hobby"/>
+</p>
+<input type ="submit" value="submit this form by clicking here"/>
+</form>
+</body>
+</html>
---
+++ AdmissionProject2/src/com/jspiders/controller/AdmissionController.java
@@ -0,0 +1,33 @@
+package com.jspiders.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+
+public class AdmissionController {
+	@RequestMapping(value="/admissionForm.html",method= RequestMethod.GET)	
+	public ModelAndView helloworld() {
+		
+		
+		ModelAndView mav=new ModelAndView("AdmissionForm");
+		
+		return mav;
+	}
+	
+	@RequestMapping(value="/submitAdmissionForm.html",method= RequestMethod.POST)	
+	public ModelAndView helloworld1(@ModelAttribute("student") Student student11233) {
+		
+		
+		ModelAndView mav=new ModelAndView("AdmissionSuccess");
+	
+		mav.addObject("msg","Details submitted by you :");
+		
+		return mav;
+	}
+
+}
